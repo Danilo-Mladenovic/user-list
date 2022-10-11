fce90a1fedeceb3bd8bbdeaa83429bcf8433a057
Danilo-Mladenovic <danilo.mladenovic999@gmail.com>
2022-10-11 21:50:38 +0200

    Added a portal for the modal.

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,28 +1,49 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 import Wrapper from "../Helpers/Wrapper";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.resetError} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+
+      <footer className={classes.actions}>
+        <Button className={classes.button1} onClick={props.resetError}>
+          Okay
+        </Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
     <Wrapper>
-      <div className={classes.backdrop} onClick={props.resetError} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.errorMessage.title}</h2>
-        </header>
-
-        <div className={classes.content}>
-          <p>{props.errorMessage.message}</p>
-        </div>
-
-        <footer className={classes.actions}>
-          <Button className={classes.button1} onClick={props.resetError}>
-            Okay
-          </Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop resetError={props.resetError} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.errorMessage.title}
+          message={props.errorMessage.message}
+          resetError={props.resetError}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </Wrapper>
   );
 };
