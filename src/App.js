d9bc655ac3240bcfeee6b0d79b5b1c63573209ca
Danilo-Mladenovic <danilo.mladenovic999@gmail.com>
2022-10-17 12:40:37 +0200

    Added a navigation.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
 
 import Users from "./components/Users/Users";
+import Login from "./components/Login/Login";
+import MainHeader from "./components/MainHeader/MainHeader";
+import Wrapper from "./components/Helpers/Wrapper";
 
 function App() {
-  return <Users />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <Wrapper>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Users onLogout={logoutHandler} />}
+      </main>
+    </Wrapper>
+  );
 }
 
 export default App;
